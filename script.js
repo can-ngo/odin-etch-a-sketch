be4e0300b581f2a9ac1cd4bc00db087a822d9f56
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-28 12:21:18 +0700

    fix: choose sizeand reload image

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const catUrl = 'https://cataas.com/cat'
 const btnSize = document.querySelector('#btn-size');
 const btnReset = document.querySelector('#btn-reset');
 const board = document.querySelector('.container');
@@ -6,35 +5,9 @@ const boardWidth = board.offsetWidth; // 912 by default
 let pixels = 16 //by Default
 let pixelWidth = boardWidth / pixels; // 57 by default
 
-reloadBackground();
-
-drawGrid(pixels, pixelWidth);
-
-btnSize.addEventListener('click', () => {
-    location.href = location.href;
-    reloadBackground();
-    board.replaceChildren();
-    pixels = prompt('Choose between 10 - 400');
-    while (pixels > 400) {
-        pixels = prompt('Choose between 10 - 400');
-    } 
-    pixelWidth = boardWidth / pixels;
-    drawGrid(pixels, pixelWidth);
-})
-
-btnReset.addEventListener('click', () => {
-    board.replaceChildren();
-    drawGrid(pixels, pixelWidth);
-    location.href = location.href;
-    reloadBackground();
-})
-
-board.addEventListener('mousemove', e => {
-    e.target.style.backgroundColor = '';
-})
-
 function reloadBackground () {
-    board.style.backgroundImage = `url(${catUrl})`;
+    let catUrl = 'https://cataas.com/cat'
+    board.style.backgroundImage = `url(${catUrl}?timestamp=${new Date().getTime()}`;
     board.style.backgroundSize = 'cover';
     board.style.backgroundPosition = 'center'; 
 }
@@ -48,4 +21,29 @@ function drawGrid (inputPixels, inputPixelWidth) {
         pixel.style.backgroundColor = 'black';
     }
 }
+
+reloadBackground();
+drawGrid(pixels, pixelWidth);
+
+btnSize.addEventListener('click', () => {  
+    reloadBackground();
+    board.replaceChildren();
+    pixels = prompt('Choose between 10 - 400');
+    while (pixels > 400) {
+        pixels = prompt('Choose between 10 - 400');
+    } 
+    pixelWidth = boardWidth / pixels;
+    drawGrid(pixels, pixelWidth);
+})
+
+btnReset.addEventListener('click', () => {
+    board.replaceChildren();
+    drawGrid(pixels, pixelWidth);
+    reloadBackground();
+})
+
+board.addEventListener('mousemove', e => {
+    e.target.style.backgroundColor = '';
+})
+
 
